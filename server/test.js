c4c480f6291789e3da0c9c4397406a852748725c
wej049 <damon.jiang@usask.ca>
2021-11-20 17:27:50 -0600

    delete

---
+++ server/test.js
@@ -0,0 +1,10 @@
+const { clearScreenDown } = require("readline");
+
+
+const { timeStamp } = require("console");
+
+var a = new Date();
+var time = a.getHours();
+var t = a.toLocaleTimeString();
+console.log(t);
+console.log(time);
